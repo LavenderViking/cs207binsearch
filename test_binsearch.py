d635164dcc8acf85df36ec6991e6c66b404661b1
Ima A. Student <tgudmundsson@g.harvard.edu>
2016-09-30 12:49:47 -0400

    17th test

--- test_binsearch.py
+++ test_binsearch.py
@@ -7,7 +7,7 @@ from binsearch import binary_search
 class Test(unittest.TestCase):
 
 	def foundElement(self):
-		self.assertEqual(binary_search(list(range(5), 2), 2)
+		self.assertEqual(binary_search(list(range(5)), 2), 2)
 
 	#def notFoundElement(self):
 	#	self.assertEqual(binary_search(list(range(5), 7), -1)
