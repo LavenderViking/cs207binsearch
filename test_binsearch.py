fabc80c4400b6a7a0ea85a09efd569bfc7ac4bd6
Ima A. Student <tgudmundsson@g.harvard.edu>
2016-09-30 12:03:01 -0400

    4th test

--- test_binsearch.py
+++ test_binsearch.py
@@ -1,10 +1,18 @@
+import unittest
+import numpy as np
 from pytest import raises
 from doctest import run_docstring_examples as dtest
 from binsearch import binary_search
 
 
+class MyTest(unittest.TestCase):
 
-def test_binsearch():
-    dtest(binary_search, globals(), verbose=True)
-    #assert myaverage([9,3]) == 6
+	def test_found(self):
+		sorted_num_list = list(range(15))
+		self.assertEqual(binary_search(sorted_num_list, 2), 2)
+
+
+	def test_binsearch():
+   		dtest(binary_search, globals(), verbose=True)
+    		#assert myaverage([9,3]) == 6
 
