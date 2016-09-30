27f591c702506976af0a8b2f655441950c4a5696
Ima A. Student <tgudmundsson@g.harvard.edu>
2016-09-30 11:43:55 -0400

    first test

--- test_binsearch.py
+++ test_binsearch.py
@@ -1,4 +1,10 @@
-from binsearch import binary_search
+from pytest import raises
 from doctest import run_docstring_examples as dtest
-dtest(binary_search, globals(), verbose=True)
+from binsearch import binary_search
+
+
+
+def test_binsearch():
+    dtest(binary_search, globals(), verbose=True)
+    #assert myaverage([9,3]) == 6
 
