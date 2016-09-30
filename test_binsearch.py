ecb0280eced052dc20fdde5787962ea89e3af3e5
Ima A. Student <tgudmundsson@g.harvard.edu>
2016-09-30 12:50:21 -0400

    18th test

--- test_binsearch.py
+++ test_binsearch.py
@@ -9,14 +9,14 @@ class Test(unittest.TestCase):
 	def foundElement(self):
 		self.assertEqual(binary_search(list(range(5)), 2), 2)
 
-	#def notFoundElement(self):
-	#	self.assertEqual(binary_search(list(range(5), 7), -1)
+	def notFoundElement(self):
+		self.assertEqual(binary_search(list(range(5)), 7), -1)
 
-	#def foundElementInRange(self):
-	#	self.assertEqual(binary_search(list(range(10), 5,1,8), 6)
+	def foundElementInRange(self):
+		self.assertEqual(binary_search(list(range(10)),5,1,8), 6)
 
-	#def notFoundElementInRange(self):
-	#	self.assertEqual(binary_search(list(range(10), 2,5,8), -1)
+	def notFoundElementInRange(self):
+		self.assertEqual(binary_search(list(range(10)), 2,5,8), -1)
 	
 
 t = unittest.TestLoader().loadTestsFromModule(Test())
