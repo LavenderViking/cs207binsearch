62724e22ebfe1f1b149348e18eca2fc1723b93bf
Ima A. Student <tgudmundsson@g.harvard.edu>
2016-09-30 12:12:49 -0400

    6th test

--- test_binsearch.py
+++ test_binsearch.py
@@ -7,4 +7,6 @@ from binsearch import binary_search
 
 class MyTest(unittest.TestCase):
 
-
+	def test_foundd(self):
+		sorted_num_list = list(range(15))
+		self.assertEqual(binary_search(sorted_num_list, 2), 2)
