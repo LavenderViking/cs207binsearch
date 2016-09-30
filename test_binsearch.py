b320e770d966ca776708e6cc639b22569b42da07
Ima A. Student <tgudmundsson@g.harvard.edu>
2016-09-30 12:35:04 -0400

    13th test

--- test_binsearch.py
+++ test_binsearch.py
@@ -9,9 +9,11 @@ class Test(unittest.TestCase):
 	def foundElement(self):
 		self.assertEqual(binary_search(list(range(5), 2), 2)
 
+
+
 	#def notFoundElement(self):
 	#	self.assertEqual(binary_search(list(range(5), 7), -1)
 
 
-ttest = unittest.TestLoader().loadTestsFromModule(MyTest())
-unittest.TextTestRunner().run(ttest)
+t = unittest.TestLoader().loadTestsFromModule(Test())
+unittest.TextTestRunner().run(t)
