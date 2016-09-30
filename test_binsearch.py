99691ee561f774a43c1122debb21dcf7036ed16c
Ima A. Student <tgudmundsson@g.harvard.edu>
2016-09-30 12:09:45 -0400

    5th test

--- test_binsearch.py
+++ test_binsearch.py
@@ -7,12 +7,4 @@ from binsearch import binary_search
 
 class MyTest(unittest.TestCase):
 
-	def test_found(self):
-		sorted_num_list = list(range(15))
-		self.assertEqual(binary_search(sorted_num_list, 2), 2)
-
-
-	def test_binsearch():
-   		dtest(binary_search, globals(), verbose=True)
-    		#assert myaverage([9,3]) == 6
 
