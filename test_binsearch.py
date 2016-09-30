7d1c8baf03c6c7885a80b1ad0d15a14ff151ba87
Ima A. Student <tgudmundsson@g.harvard.edu>
2016-09-30 12:15:59 -0400

    8th test

--- test_binsearch.py
+++ test_binsearch.py
@@ -9,3 +9,7 @@ class MyTest(unittest.TestCase):
 	def test_foundd(self):
 		sorted_num_list = list(range(15))
 		self.assertEqual(binary_search(sorted_num_list, 2), 2)
+
+
+suite = unittest.TestLoader().loadTestsFromModule(MyTest())
+unittest.TextTestRunner().run(suite)
