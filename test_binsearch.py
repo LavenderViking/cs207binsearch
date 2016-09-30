928863ba7331d5b36fad3128ea646f59c5da1a91
Ima A. Student <tgudmundsson@g.harvard.edu>
2016-09-30 12:15:33 -0400

    7th test

--- test_binsearch.py
+++ test_binsearch.py
@@ -1,8 +1,7 @@
 import unittest
-import numpy as np
 from pytest import raises
-from doctest import run_docstring_examples as dtest
-from binsearch import binary_search
+import numpy as np
+from binarysearch import binary_search
 
 
 class MyTest(unittest.TestCase):
